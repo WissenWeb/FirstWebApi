71b01c3d41489f898649eec42bfc2ec4f6f2eaab
BRIGHT <wissen30@hotmail.com>
2023-11-22 21:51:48 +0300

    Add project files.

---
+++ FirstWebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+    };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public int Get()
+        {
+
+            return 10;
+        }
+
+        [HttpPost]
+        public int Post(int a,int b)
+        {
+
+            return a + b;
+
+        }
+       
+    }
+}
---
+++ FirstWebApi/Controllers/WissenController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Data;
+
+namespace FirstWebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WissenController : ControllerBase
+    {
+
+        [HttpGet]
+        public int Index()
+        {
+            return 18;
+        }
+
+        [HttpPost]
+        public string Post(string deger) {
+
+            string newvalue = "";
+            for (int i = deger.Length - 1; i >= 0; i--)
+            {
+                newvalue += deger[i];
+            }
+            return newvalue;
+           
+
+        }
+        
+    }
+}
